bb9b6e059e2181e468954bc662e14030c95d908d
Ann4530 <anhhphe186087@fpt.edu.vn>
2024-09-24 17:22:22 +0700

    Merge branch 'master' into anhHPHE186087

--- src/java/controller/HomepageServlet.java
+++ src/java/controller/HomepageServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.*;
@@ -21,34 +20,37 @@ import model.Subject;
  * @author ADMIN
  */
 public class HomepageServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet HomepageServlet</title>");  
+            out.println("<title>Servlet HomepageServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet HomepageServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet HomepageServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,24 +58,27 @@ public class HomepageServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         //post_list
-        PostDAO myPostDAO=new PostDAO();
-        ArrayList<Post> post_list=myPostDAO.getPost();
+        dal.PostDAO myPostDAO = new dal.PostDAO();
+        ArrayList<Post> post_list = myPostDAO.getPost();
         request.setAttribute("post_list", post_list);
-        
+
+        //hottest_post_list đang lỗi!!!
+        ArrayList<Post> hottest_post_list = myPostDAO.getHottestPost();
+        request.setAttribute("hottest_post_list", hottest_post_list);
+
         //subject_list
-        SubjectDAO mySubjectDAO=new SubjectDAO();
-        ArrayList<Subject> subject_list=mySubjectDAO.getSubject();
+        SubjectDAO mySubjectDAO = new SubjectDAO();
+        ArrayList<Subject> subject_list = mySubjectDAO.getSubject();
         request.setAttribute("subject_list", subject_list);
         
         request.getRequestDispatcher("homepage.jsp").forward(request, response);
-                
-        
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -81,12 +86,13 @@ public class HomepageServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
