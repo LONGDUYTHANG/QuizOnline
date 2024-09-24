4a9758d9f238f9845b5c360b2ec15d7edb0ecf17
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2024-09-25 00:24:33 +0700

    add access modifiers

--- src/java/model/Category.java
+++ src/java/model/Category.java
@@ -9,8 +9,8 @@ package model;
  * @author ADMIN
  */
 public class Category {
-    int category_id;
-    String category_name;
+    private int category_id;
+    private String category_name;
 
     public Category() {
     }
