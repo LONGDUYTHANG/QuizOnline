9a3977645533cf71ae55100837a91695c6bf85ff
thangldhe186175 <thangldhe186175@fpt.edu.vn>
2024-09-26 13:06:43 +0700

    fixed a lot of things

--- src/java/controller/AddNewQuizValidationServlet.java
+++ src/java/controller/AddNewQuizValidationServlet.java
@@ -20,7 +20,7 @@ import model.Subject;
  *
  * @author FPT SHOP
  */
-public class AddQuizValidationServlet extends HttpServlet {
+public class AddNewQuizValidationServlet extends HttpServlet {
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
