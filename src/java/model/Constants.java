1c63ca7afd6af5cf75d7c637b9f3235ae716ab36
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2024-09-26 14:45:55 +0700

    modified Constants

--- src/java/model/Constants.java
+++ src/java/model/Constants.java
@@ -11,7 +11,7 @@ package model;
 public class Constants {
     public static String GOOGLE_CLIENT_ID = "393624416817-8crlnf1o21kjk410epg6pjg7q6bohd79.apps.googleusercontent.com";
   public static String GOOGLE_CLIENT_SECRET = "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";
-  public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/LoginbyFace/login";
+  public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/quizonline/googlelogin";
   public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
   public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
   public static String GOOGLE_GRANT_TYPE = "authorization_code";
