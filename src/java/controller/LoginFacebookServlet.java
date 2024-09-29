2546425de035b38ffc437c4658ec3ed14b748b4b
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2024-09-29 17:26:34 +0700

    modidied for login with facebook

--- src/java/controller/LoginFacebookServlet.java
+++ src/java/controller/LoginFacebookServlet.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
+import dal.AccountDAO;
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,6 +12,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Account;
 import model.GooglePojo;
 import model.RestFB;
 
@@ -20,34 +21,37 @@ import model.RestFB;
  * @author ADMIN
  */
 public class LoginFacebookServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginFacebookServlet</title>");  
+            out.println("<title>Servlet LoginFacebookServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginFacebookServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet LoginFacebookServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -55,24 +59,29 @@ public class LoginFacebookServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-                 String code = request.getParameter("code");
-    
-    if (code == null || code.isEmpty()) {
-      RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
-      dis.forward(request, response);
-    } else {
-      String accessToken = RestFB.getToken(code);
-        GooglePojo user = RestFB.getUserInfo(accessToken);
-      request.setAttribute("id", user.getId());
-      request.setAttribute("name", user.getEmail());
-      RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
-      dis.forward(request, response);
-    }
-    } 
+            throws ServletException, IOException {
+        String code = request.getParameter("code");
 
-    /** 
+        if (code == null || code.isEmpty()) {
+            RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+            dis.forward(request, response);
+        } else {
+            String accessToken = RestFB.getToken(code);
+            GooglePojo user = RestFB.getUserInfo(accessToken);
+            AccountDAO myAccountDAO = new AccountDAO();
+            Account myAccount = myAccountDAO.getAccount(user.getEmail());
+            //if email has not been registered, add it to the database 
+            if (myAccount != null) {
+                myAccountDAO.addAccount(user.getEmail(), "default password");
+            }
+            RequestDispatcher dis = request.getRequestDispatcher("customer/homepage_1.jsp");
+                dis.forward(request, response);
+        }
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -80,12 +89,13 @@ public class LoginFacebookServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         doGet(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
