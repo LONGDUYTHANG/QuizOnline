03b32d6fdd41afd6364473809c26124cf37bdfb4
SonNTHE181860 <147047912+sonnthe@users.noreply.github.com>
2024-09-30 22:20:42 +0700

    Update SubjectDAO.java

--- src/java/dal/SubjectDAO.java
+++ src/java/dal/SubjectDAO.java
@@ -333,40 +333,38 @@ public class SubjectDAO extends DBContext {
 
         return subjects;
     }
-    public Account getAccountById1(String accountId) {
-        String sql = "SELECT account_id, full_name, gender, email, mobile, password, avatar, role_id "
-                + "FROM Account WHERE account_id = ?";
+    public List<Subject> getAllSubject1() {
+        List<Subject> subjects = new ArrayList<>();
+        String sql = "SELECT subject_id, subject_name, category_id, status, isFeatured, thumbnail, tagline, description, account_id, created_date FROM Subject";
 
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            // Set the account ID parameter
-            pstmt.setString(1, accountId);
+        try (
+                PreparedStatement pstmt = connection.prepareStatement(sql); ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                Subject subject = new Subject();
+                subject.setSubject_id(rs.getInt("subject_id"));
+                subject.setSubject_name(rs.getString("subject_name"));
 
-            // Execute the query
-            ResultSet rs = pstmt.executeQuery();
+                CategoryDAO cDao = new CategoryDAO();
+                SubjectCategory sc = cDao.getCategoryById(rs.getInt("category_id"));
+                subject.setCategory(sc);
 
-            // If a record is found, map it to the Account object
-            if (rs.next()) {
-                Account account = new Account();
-                account.setAccount_id(rs.getInt("account_id"));
-                account.setFull_name(rs.getString("full_name"));
-                account.setGender1(rs.getInt("gender")); // Assuming gender is stored as a boolean
-                account.setEmail(rs.getString("email"));
-                account.setMobile(rs.getString("mobile"));
-                account.setPassword(rs.getString("password"));
-                account.setAvatar(rs.getString("avatar"));
+                subject.setStatus1(rs.getInt("status"));
+                subject.setIs_featured(rs.getBoolean("isFeatured"));
+                subject.setThumbnail(rs.getString("thumbnail"));
+                subject.setTag_line(rs.getString("tagline"));
+                subject.setDescription(rs.getString("description"));
 
-                AccountDAO adao = new AccountDAO();
-                Role role = adao.getRoleById(rs.getInt("role_id"));
+                AccountDAO aDao = new AccountDAO();
+                Account acc = aDao.getAccountById1(rs.getString("account_id"));
+                subject.setAccount_id(acc);
+                subject.setCreated_date(rs.getDate("created_date"));
 
-                account.setRole_id1(role);
-
-                return account; 
+                subjects.add(subject);
             }
-
         } catch (SQLException e) {
-            e.printStackTrace(); 
+            e.printStackTrace();
         }
 
-        return null;
+        return subjects;
     }
 }
