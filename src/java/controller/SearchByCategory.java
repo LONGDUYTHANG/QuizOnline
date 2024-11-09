e5f7d2ea3dcf1b43790cf7b73c7518e097ae287a
SonNTHE181860 <147047912+sonnthe@users.noreply.github.com>
2024-11-09 16:19:50 +0700

    Update SearchByCategory.java

--- src/java/controller/SearchByCategory.java
+++ src/java/controller/SearchByCategory.java
@@ -103,6 +103,12 @@ public class SearchByCategory extends HttpServlet {
         List<Post> hottest_post_list = myPostDAO.getHottestPost();
         request.setAttribute("hottest_post_list", hottest_post_list);
 
+                AccountDAO accountDAO = new AccountDAO();
+        List<Account> account_list = new ArrayList<>();
+        for (Post blog : post_list) {
+            Account account = accountDAO.getAccountById(blog.getAccount_id());
+            account_list.add(account);
+        }
         List<Category> category_list;
         if (keyword != null && !keyword.isEmpty()) {
             category_list = myCategoryDAO.searchCategories(keyword);
